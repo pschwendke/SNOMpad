42de7d9f255ffc6092e415fb545b07e79e45abbd
SPalato <palato@fhi-berlin.mpg.de>
2020-03-25 20:26:31 +0100

    Now with tqdm support. Next: try some stream

--- scratch/acquire_finite.py
+++ scratch/acquire_finite.py
@@ -1,12 +1,12 @@
 # try a finite acquisition
 import numpy as np
-#import tqdm
+from tqdm import tqdm
 import nidaqmx as ni
 from nidaqmx.constants import VoltageUnits
 from nidaqmx.constants import AcquisitionType
 from nidaqmx.stream_readers import AnalogMultiChannelReader, AnalogSingleChannelReader
 import logging
-from time import time
+from time import time, sleep
 logging.basicConfig(level=logging.DEBUG)
 
 def task_done_cb(task, status, data):
@@ -15,10 +15,9 @@ def task_done_cb(task, status, data):
     logging.info(f"data: {type(data)}, {data}")
     return 0
 
-def n_samp_acq_cb(task, event_type, n, data):
-    print(f"progress: {n}")
-    return 0
 
+
+n_samp = 100000
 # let's acquire 3 channels, 200 khz, 100 000 times
 with ni.Task("signals") as task:
     # NIDAQmx indexes by 0 but includes endpoint, ie: ai0:2 -> ai0, ai1, ai2
@@ -27,28 +26,42 @@ with ni.Task("signals") as task:
         min_val=-10, max_val=10,
         units=VoltageUnits.VOLTS,
     ) 
-    task.register_done_event(task_done_cb) # this now requires explicit call to start
-    task.register_every_n_samples_acquired_into_buffer_event(10000, n_samp_acq_cb)
+    n_channels = task.number_of_channels
+    #task.register_done_event(task_done_cb) # this now requires explicit call to start
     task.timing.cfg_samp_clk_timing(
         rate=200000,
         # source= If '', use onboard clock
         sample_mode=AcquisitionType.FINITE,
-        samps_per_chan=100000
+        samps_per_chan=n_samp,
     )
-    logging.info("Setting up reader")
-    #tmg = task.timing
-    #logging.info(f"Timing: {tmg}")
-    #trg = task.triggers
-    #logging.info(f"Trigger: {trg}")
-    logging.info("Starting task")
-    task.start()
-    t0 = time()
-    task.wait_until_done()
-    dt = time()-t0
+    with tqdm(total=n_samp) as pbar:
+        def n_samp_acq_cb(task, event_type, n, data):
+            # there is a bug with tqdm: it is sometimes shown when pbar context
+            # manager exits. It is not due to nidaqmx firing the event at an
+            # unexpected moment.
+            pbar.update(n)
+            return 0
+        
+        task.register_every_n_samples_acquired_into_buffer_event(10000, n_samp_acq_cb)
+
+        #tmg = task.timing
+        #logging.info(f"Timing: {tmg}")
+        #trg = task.triggers
+        #logging.info(f"Trigger: {trg}")
+
+        #logging.info("Starting task")
+        task.start()
+        t0 = time()
+        task.wait_until_done()
+        
+        v = task.read() #number_of_samples_per_channel=10)
+        task.stop()
+        dt = time()-t0
     logging.info(f"took: {dt} s")
-    # implicit: stop()
-    v = task.read()#number_of_samples_per_channel=10)
-    
     logging.info(f"Value len: {len(v)}")
     logging.info(f"Typeof: {type(v)}")
     logging.info(f"Done? {task.is_task_done()}")
+    logging.info("Finishing task...")
+
+print("Script finished")
+
