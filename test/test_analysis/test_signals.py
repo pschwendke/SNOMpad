dc767c70191e768f2fcf2fe586d3bd06abd13803
SPalato <palato@fhi-berlin.mpg.de>
2020-05-13 11:16:03 +0200

    Basic testing of signal machinery.

--- test/test_analysis/test_signals.py
+++ test/test_analysis/test_signals.py
@@ -1,3 +1,32 @@
-from trion.analysis.signals import Signals, Experiment, Scan, Enum
+from trion.analysis.signals import (
+    Signals, Experiment, Scan, Acquisition, Detector
+)
+import pytest
 
-raise NotImplementedError()
+def test_sortable():
+    assert len(list(Signals)) == len(Signals.__members__) # make sure these are actually the members
+    assert sorted(Signals) == list(Signals)
+
+exp_configs = {
+    "shd single": (
+        Experiment(Scan.point, Acquisition.shd, Detector.single),
+        {Signals.sig_A, Signals.tap_x, Signals.tap_y}
+    ),
+    "shd dual": (
+        Experiment(Scan.point, Acquisition.shd, Detector.dual),
+        {Signals.sig_A, Signals.sig_B, Signals.tap_x, Signals.tap_y}
+    )
+
+
+}
+@pytest.mark.parametrize(
+    "exp, signals",
+    exp_configs.values(),
+    ids=exp_configs.keys(),
+)
+def test_expt_configs(exp, signals):
+    # test the behavior of currently supported experimental configurations.
+    assert exp.is_valid()
+    res = exp.signals()
+    assert sorted(res) == res
+    assert set(res) == signals
