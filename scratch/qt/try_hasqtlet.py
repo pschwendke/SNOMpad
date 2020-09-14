6a696fd70e3fe5a64a08f1cce68e8ee472a12af8
SPalato <palato@fhi-berlin.mpg.de>
2020-09-14 21:03:42 +0200

    The HasQtlets class seems to be working decently.

--- scratch/qt/try_hasqtlet.py
+++ scratch/qt/try_hasqtlet.py
@@ -1,37 +1,33 @@
-#
-
 import sys
 from random import randint
 
 from PySide2.QtWidgets import QWidget, QPushButton, QVBoxLayout, QApplication
-from traitlets import HasTraits, Integer
+from traitlets import Integer
 
 from trion.expt.gui.utils import IntEdit
-from trion.expt.gui.qtlets import Qtlet, IntQtlet, HasQtlets
+from trion.expt.gui.qtlets import HasQtlets
 
 class Data(HasQtlets):
     value = Integer(default_value=1, min=0, max=10)
+
 
 class Form(QWidget):
     def __init__(self, parent=None, data=None):
         super().__init__(parent)
         self.data = data
-        self.edit = IntEdit("...")
 
+        self.edit = IntEdit("...")
         self.otheredit = IntEdit("???")
         #self.otheredit.setEnabled(False)
         self.button = QPushButton("Roll!")
 
         layout = QVBoxLayout()
-        layout.addWidget(self.edit)
-        layout.addWidget(self.otheredit)
-        layout.addWidget(self.button)
+        for w in [self.edit, self.otheredit, self.button]:
+            layout.addWidget(w)
         self.setLayout(layout)
 
         data.link_widget(self.edit, "value")
         data.link_widget(self.otheredit, "value")
-
-        # set the initial value, streamline this at initial connection
 
         self.button.clicked.connect(self.on_btn_click)
         self.setWindowTitle("Directional connection")
@@ -48,11 +44,9 @@ def update_cb(change):
 
 
 if __name__ == '__main__':
-    # Create the Qt Application
     app = QApplication(sys.argv)
     d = Data(value=3)
     d.observe(update_cb, names="value")
     form = Form(data=d)
     form.show()
-    # Run the main Qt loop
     sys.exit(app.exec_())
