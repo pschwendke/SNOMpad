4a7d3b3e36ba6e2d958649b3c63a6cbde61ff0ff
SPalato <palato@fhi-berlin.mpg.de>
2020-10-20 17:10:08 +0200

    added script for transfer function measurement.

--- src/trion/expt/acquisition.py
+++ src/trion/expt/acquisition.py
@@ -1,13 +1,17 @@
 # acquisition scripts
 import logging
 from time import sleep
-from typing import Iterable
+from typing import Iterable, Sized
 import numpy as np
+from tqdm import tqdm
 
 from trion.analysis.signals import Signals
 from trion.expt.buffer import ExtendingArrayBuffer
 from trion.expt.daq import DaqController
 
+import nidaqmx
+from nidaqmx.constants import (
+    Edge, TaskMode)
 
 def single_point(device: str, signals: Iterable[Signals], n_samples: int,
                  clock_channel: str="", truncate: bool=False, pbar=None, ):
@@ -58,3 +62,82 @@ def single_point(device: str, signals: Iterable[Signals], n_samples: int,
     if truncate and np.isfinite(n_samples):
         data = data[:n_samples,:]
     return data
+
+
+def transfer_func_acq(
+        device: str,
+        read_channels: Iterable[str], write_channels: Iterable[str],
+        freqs, amp: float, offset: float=0,
+        n_samples: int=int(1E3), sample_rate: float=1E6,
+        pbar=None, logger=None,
+):
+    """
+    Perform transfer function measurement using a slow passage method.
+    """
+    if logger is None:
+        logger = logging.getLogger(__name__)
+    t = np.arange(0, n_samples)/sample_rate
+    tail_len = int(5E-4*sample_rate) # 500 us of tail
+    measured = []
+    with nidaqmx.Task("write") as write_task, nidaqmx.Task("read") as read_task, \
+            nidaqmx.Task("clock") as sample_clk_task:
+        # prepare sample clock
+        logger.debug("Setting up clock task.")
+        sample_clk_task.co_channels.add_co_pulse_chan_freq(
+            f'{device}/ctr0', freq=sample_rate)
+        sample_clk_task.timing.cfg_implicit_timing(
+            samps_per_chan=n_samples + tail_len)
+        sample_clk_task.control(TaskMode.TASK_COMMIT)
+        sample_clk_terminal = f"/{device}/Ctr0InternalOutput"
+
+        # prepare write task
+        logger.debug("Setting up output task.")
+        write_task.ao_channels.add_ao_voltage_chan(
+            [device+"/"+c for c in write_channels], max_val=5, min_val=-5,
+        )
+        write_task.timing.cfg_samp_clk_timing(
+            sample_rate, source=sample_clk_terminal,
+            active_edge=Edge.RISING,
+            samps_per_chan=n_samples + tail_len)
+
+        # prepare read task
+        logger.debug("Setting up input task.")
+        read_task.ai_channels.add_ai_voltage_chan(
+            [device+"/"+c for c in read_channels], max_val=5, min_val=-5,
+        )
+        read_task.timing.cfg_samp_clk_timing(
+            sample_rate, source=sample_clk_terminal,
+            active_edge=Edge.RISING, samps_per_chan=n_samples)
+
+        tail = np.ones((write_task.number_of_channels, tail_len)) * offset
+        if pbar is None:
+            pbar = tqdm(freqs, disable=True)
+        for target_freq in pbar:
+            y = amp * np.sin(t * 2 * np.pi * target_freq) + offset
+            payload = np.repeat(y.reshape((1, -1)),
+                                write_task.number_of_channels,
+                                axis=0)
+            payload = np.hstack((payload, tail))
+            assert payload.shape == (write_task.number_of_channels, n_samples + tail_len)
+
+            write_task.write(payload)
+            logger.info(f"Measuring f={target_freq:5.02e}")
+
+            read_task.start()
+            write_task.start()
+            sample_clk_task.start()
+
+            values_read = np.array(read_task.read(
+                number_of_samples_per_channel=n_samples, timeout=2))
+
+            read_task.stop()
+            write_task.stop()
+            sample_clk_task.stop()
+
+            measured.append(values_read)
+
+    logger.info("Done...")
+    measured = np.array(measured)
+    assert measured.shape == (freqs.size, read_task.number_of_channels, n_samples)
+    return measured
+
