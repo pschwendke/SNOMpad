cd61780529c56401641cba4ba55830701babf118
SPalato <palato@fhi-berlin.mpg.de>
2020-10-21 15:30:00 +0200

    Reorganized buffer submodule.

--- src/trion/expt/buffer/array.py
+++ src/trion/expt/buffer/array.py
@@ -1,135 +1,11 @@
-#buffer.py: Acquisition buffers
-"""
-Buffers: Manage the output buffer.
-    Handle opening, sync (flushing), closing, expansion, truncation. Must track
-    its current index to enable reading by other classes without involving the
-    reader. Must handle two read modes: manually put data and 
-    provide view into buffer.
-    The destination buffer could also be the entrance into an analysis pipeline...
-
-NOT THREADSAFE.
-"""
-from abc import ABC, abstractmethod
-import logging
 from math import ceil
+import logging
 
 import numpy as np
-import os.path as pth
-from ..analysis.io import export_data
+
+from trion.expt.buffer import AbstractBuffer
+
 logger = logging.getLogger(__name__)
-
-
-class AbstractBuffer(ABC):
-    def __init__(self, *, vars):
-        """
-        Defines the API for Buffer objects.
-
-        Parameters
-        ----------
-        vars : iterable of Signals
-
-
-        Attributes
-        ----------
-        i : int
-            Index of next value to be written
-        size : int, read-only
-            Current size of buffer.
-        """
-        self.i = 0
-        self._vrs = vars
-
-    @property
-    @abstractmethod
-    def size(self):
-        """
-        Current buffer size.
-        """
-        pass
-
-    @abstractmethod
-    def close(self) -> 'AbstractBuffer':
-        """
-        Close the buffer.
-        """
-        pass
-
-    @abstractmethod
-    def finish(self) -> 'AbstractBuffer':
-        """
-        Cleanup after acquisition finished.
-        """
-        pass
-
-
-    @abstractmethod 
-    def put(self, data) -> 'AbstractBuffer':
-        """
-        Put data into buffer. Handles expansion if necessary.
-
-        Parameters
-        ----------
-        data : array_like of shape (N,M)
-            Data to be added. N points with M variables.
-        """
-        pass
-
-    @abstractmethod
-    def get(self, len: int, offset: int=0):
-        """
-        Get valid values from buffer.
-
-        This method does not return values that may be present in the buffer
-        but were not written (ie: allocated space).
-
-        Parameters
-        ----------
-        len : int
-            Number of points to return.
-        offset : int
-            Start position
-
-        Returns
-        -------
-        data : np.ndarray of shape (N,M)
-            Data points of up to `len` points and `M` columns.
-        """
-        pass
-
-    def tail(self, len):
-        """
-        Get `len` last values from buffer.
-        """
-        offset = self.i-len
-        return self.get(len, offset)
-
-    def head(self, len):
-        """
-        Get `len` values from start of buffer.
-        """
-        return self.get(len)
-
-    @property
-    def vars(self):
-        """Get the vars names present in buffer (ie: columns)"""
-        return self._vrs
-
-    def export(self, filename, len: int=None):
-        """
-        Export buffer contents to file.
-
-        Parameters
-        ----------
-        filename: str or path-like
-            Output file name
-        len: int or None (default)
-            Number of points to export, counting from end. If len is None
-            (default), exports the entire buffer.
-        """
-        self.finish()
-        len = self.size if (len is None) else len
-        export_data(filename, self.tail(len), self.vars)
-
 
 class ArrayBuffer(AbstractBuffer):
     def __init__(self, *, size, dtype=np.float, **kw):
@@ -248,7 +124,7 @@ class ExtendingArrayBuffer(ArrayBuffer):
     @property
     def size(self):
         return self.i
-    
+
     def put(self, data):
         n = np.asarray(data).shape[0]
         j = self.i+n
@@ -277,12 +153,12 @@ class ExtendingArrayBuffer(ArrayBuffer):
 
     def finish(self):
         self.truncate()
-        return self    
-    
+        return self
+
     def close(self):
         self.finish()
         return self
-    
+
     def truncate(self):
         logger.debug(f"Truncating buffer to: {self.i}")
         self.buf = self.buf[:self.i,:]
@@ -300,6 +176,3 @@ class ExtendingArrayBuffer(ArrayBuffer):
             ),
         )
         return self
-        
-
-# Factory function?
---
+++ src/trion/expt/buffer/h5.py
@@ -0,0 +1,10 @@
+"""
+Acquitision buffer based on H5Py.
+
+
+"""
+import logging
+
+from .base import AbstractBuffer
+
+logger = logging.getLogger(__name__)
