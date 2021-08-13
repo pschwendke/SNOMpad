bea6fb4edb6f6570de60dcfe9022933d212972c2
SPalato <7659022+spalato@users.noreply.github.com>
2021-08-13 14:52:22 +0200

    Finished renovating the demodulation functions.

--- src/trion/analysis/demod.py
+++ src/trion/analysis/demod.py
@@ -1,52 +1,11 @@
 # demod.py: functions for demodulations
 from itertools import product
-
+from warnings import warn
 import numpy as np
 import pandas as pd
 
 from .signals import is_tap_modulation
 
-
-def dft_naive(phi, y, orders):
-    """
-    Perform a naive fourier transform using trapezoidal integration.
-
-    The input arrays (phi) are sorted before integration.
-
-    Parameters
-    ----------
-    phi : (N,) np.ndarray
-        Values of phi.
-    y : (N,) np.ndarray
-        Values of y.
-    orders : (M,) np.ndarray
-        Demodulation orders
-
-    Returns
-    -------
-    amp : (M,) np.ndarray
-        Complex amplitude for the given orders.
-    """
-    assert phi.ndim == 1
-    if y.ndim == 1:
-        y = y[np.newaxis,:]
-    idx = np.argsort(phi)
-    y_s = y[:,idx]
-    phi_s = phi[idx]
-    return np.trapz(
-        np.exp(1j*phi_s[np.newaxis, :]*orders[:, np.newaxis])*y_s,
-        phi_s,
-        axis=1
-    )/2/np.pi
-
-
-def calc_phase(df, in_place=False):
-    "Computes tap_p"
-    # todo: deprecate
-    if not in_place:
-        df = df.copy()
-    df["tap_p"] = np.arctan2(df["tap_y"], df["tap_x"])
-    return df
 
 
 def bin_index(phi, n_bins: int):
@@ -60,7 +19,6 @@ def bin_index(phi, n_bins: int):
 
 def bin_midpoints(n_bins, lo=-np.pi, hi = np.pi):
     """Compute the midpoints of phase bins"""
-    # TODO: test
     span = hi - lo
     step = span/n_bins
     return (np.arange(n_bins)+0.5) * step + lo
@@ -74,6 +32,28 @@ _avg_drop_cols = set(  # tap_x, tap_y, tap_p...
 
 
 def shd_binning(df: pd.DataFrame, tap_nbins: int = 32):
+    """Perform sHD binned average on `df`.
+
+    Compute the tapping phase `tap_p` from `tap_y` and `tap_x`, then partition
+    the data points in an histogram based on `tap_p`, and compute the average
+    per bin.
+
+    The function performs some operations in-place, therefore affecting the
+    contents of `df`. To avoid this, pass a copy, ex: `shd_binning(df.copy())`
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe of sample points. The columns indicate the signal type.
+    tap_nbins : int
+        Number of bins to use. Default: 32
+
+    Returns
+    -------
+    avg : pd.DataFrame
+        Dataframe containing the average per bins. Row index contains the bin
+        number for tapping. Columns indicate the signal type.
+    """
     # TODO test
     #  smoke test passed
     # compute phases
@@ -87,8 +67,21 @@ def shd_binning(df: pd.DataFrame, tap_nbins: int = 32):
 
 
 def shd_ft(avg: pd.DataFrame):
-    """Perform Fourier analysis """
-    # Todo: test
+    """Perform Fourier analysis for sHD demodulation.
+
+    Parameters
+    ----------
+    avg : pd.DataFrame
+        Data points averaged per bins. Rows indicate bin, columns indicate
+        signal type. Note that every bin should be present, this is not checked.
+
+    Returns
+    -------
+    shd : pd.DataFrame
+        Fourier components. Rows indicate tapping order `n`, columns indicate
+        signal type.
+    """
+    # todo: add to test suite before modifying
     #  smoke test passed
     # normalization factor: step/2/np.pi, with step = 2*np.pi/len(avg)
     return avg.apply(np.fft.rfft, axis=0)/len(avg)
@@ -97,15 +90,16 @@ def shd_ft(avg: pd.DataFrame):
 def shd(df: pd.DataFrame, tap_nbins: int = 32):
     """
     Perform sHD demodulation by binning and FT.
+
+    This is a function simply calls `shd_binnning` and `shd_ft` in sequence.
     """
-    # todo: test
+    # todo: add to test suite before modifying
     #  smoke test passed
-    avg = shd_binning(df, tap_nbins)
-    return shd_ft(avg)
+    return shd_ft(shd_binning(df, tap_nbins))
 
 
 def pshet_binning(df: pd.DataFrame, tap_nbins: int = 32, ref_nbins: int = 16):
-    """Perform pshet binning on `df`.
+    """Perform pshet binned average on `df`.
 
     Partition the data points in a 2d histogram, and compute the average per
     bin. The function performs some operations in-place, therefore affecting the
@@ -114,8 +108,7 @@ def pshet_binning(df: pd.DataFrame, tap_nbins: int = 32, ref_nbins: int = 16):
     Parameters
     ----------
     df : pd.DataFrame
-        Dataframe containing the data from many samples. The different signals
-        must be in different columns.
+        Dataframe of sample points. The columns indicate the signal type.
     tap_nbins : int
         Number of bins for tapping modulation. Default: 32
     ref_nbins : int
@@ -163,8 +156,82 @@ def pshet(df: pd.DataFrame, tap_nbins: int = 32, ref_nbins: int = 16):
     return pshet_ft(avg)
 
 
-def binned_average(df, n_bins, compute_counts=True):
+def dft_lstsq(phi, sig, max_order: int):
+    assert phi.ndim == 1
+    assert max_order > 1
+    orders = np.arange(1, max_order+1)
+    coeff = np.hstack([
+        np.ones_like(phi)[:,np.newaxis], # DC
+        np.cos(orders[np.newaxis,:] * phi[:,np.newaxis]), # cos
+        np.sin(orders[np.newaxis,:] * phi[:,np.newaxis])  # sin
+    ])
+    assert coeff.shape == (phi.size, 2*max_order+1)
+    soln, _, _, _ = np.linalg.lstsq(coeff, sig, rcond=None)
+    ret = np.asarray(soln[:max_order+1], dtype=complex) # what happens if sig.ndim > 1?
+    ret[1:] += 1j*soln[max_order+1:]
+    return ret
+
+
+def dft_naive(phi, y, orders):
+    """
+    Perform a naive fourier transform using trapezoidal integration.
+
+    The input arrays (phi) are sorted before integration.
+
+    Parameters
+    ----------
+    phi : (N,) np.ndarray
+        Values of phi.
+    y : (N,) np.ndarray
+        Values of y.
+    orders : (M,) np.ndarray
+        Demodulation orders
+
+    Returns
+    -------
+    amp : (M,) np.ndarray
+        Complex amplitude for the given orders.
+    """
+    assert phi.ndim == 1
+    if y.ndim == 1:
+        y = y[np.newaxis,:]
+    idx = np.argsort(phi)
+    y_s = y[:,idx]
+    phi_s = phi[idx]
+    return np.trapz(
+        np.exp(1j*phi_s[np.newaxis, :]*orders[:, np.newaxis])*y_s,
+        phi_s,
+        axis=1
+    )/2/np.pi
+
+
+#####  older stuff, kept for compatibility
+
+_deprecation_warning = FutureWarning("This function is deprecated. Please use the `shd` and `pshet` set of functions.")
+
+def calc_phase(df, in_place=False):
+    warn(_deprecation_warning)
+    "Computes tap_p"
     # todo: deprecate
+    if not in_place:
+        df = df.copy()
+    df["tap_p"] = np.arctan2(df["tap_y"], df["tap_x"])
+    return df
+
+
+def bin_no(df, n_bins, in_place=False):
+    warn(_deprecation_warning)
+    "Computes bin_no"
+    if not in_place:
+        df = df.copy()
+    lo = -np.pi
+    step = 2*np.pi/n_bins
+    df["bin_no"] = (df["tap_p"] - lo)//step
+    return df
+
+
+def binned_average(df, n_bins, compute_counts=True):
+    warn(_deprecation_warning)
     df = df.copy()
     if not "tap_p" in df.columns:
         calc_phase(df, in_place=True)
@@ -183,8 +250,8 @@ def binned_average(df, n_bins, compute_counts=True):
     return avg
 
 
-def binned_ft(avg):  # todo: these names suck
-    # todo: deprecate
+def binned_ft(avg):  # these names suck
+    warn(_deprecation_warning)
     step = np.diff(avg["phi"].iloc[:2])[0] # argh...
     sigs = avg.drop(columns="phi")
     return pd.DataFrame(np.fft.rfft(sigs, axis=0)*step/2/np.pi, columns=sigs.columns)
@@ -199,24 +266,7 @@ def dft_binned(phi, sig, n_bins=256):
     values are used to compute the fourier components.
     """
     # Somehow very fast?!
+    warn(_deprecation_warning)
     # TODO: expand to multiple signals
     df = pd.DataFrame(np.vstack([phi, sig]).T, columns=["tap_p", "sig"])
     return np.squeeze(binned_ft(binned_average(df, n_bins, compute_counts=False)).to_numpy())
-
-
-def dft_lstsq(phi, sig, max_order: int):
-    assert phi.ndim == 1
-    assert max_order > 1
-    orders = np.arange(1, max_order+1)
-    coeff = np.hstack([
-        np.ones_like(phi)[:,np.newaxis], # DC
-        np.cos(orders[np.newaxis,:] * phi[:,np.newaxis]), # cos
-        np.sin(orders[np.newaxis,:] * phi[:,np.newaxis])  # sin
-    ])
-    assert coeff.shape == (phi.size, 2*max_order+1)
-    soln, _, _, _ = np.linalg.lstsq(coeff, sig, rcond=None)
-    ret = np.asarray(soln[:max_order+1], dtype=complex) # what happens if sig.ndim > 1?
-    ret[1:] += 1j*soln[max_order+1:]
-    return ret
-
-
