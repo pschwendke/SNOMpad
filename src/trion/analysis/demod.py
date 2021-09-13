1c1b2d0ed8560b1b00149d63c9057e4c7e86bce1
pschwendke <mail@philippschwendke.de>
2021-09-13 16:24:46 +0200

    moved empty_bins_in function to utils.py

--- src/trion/analysis/demod.py
+++ src/trion/analysis/demod.py
@@ -3,30 +3,9 @@ from itertools import product
 from warnings import warn
 import numpy as np
 import pandas as pd
+from .utils import empty_bins_in
 
 from .signals import is_tap_modulation
-
-
-def empty_bins_in(df: pd.DataFrame) -> None:
-    """Checks binned 1D or 2D DataFrame for empty bins in index (1D) and column names (2D).
-    Returns True if empty bins are detected."""
-
-    # check for missing tap bins
-    for n in range(df.shape[0]):
-        if df.index[n] != n:
-            raise ValueError('The binned DataFrame has missing bins.')
-
-    # check for missing pshet bins
-    if isinstance(df.columns, pd.MultiIndex):
-        for channel in df.columns.get_level_values(0).drop_duplicates():
-            for m in range(df[channel].shape[1]):
-                if df[channel].columns[m] != m:
-                    raise ValueError('The binned DataFrame has missing bins.')
-
-    # check for empty (NAN) bins
-    if np.isnan(df).any(axis=None):
-        raise ValueError('The binned DataFrame has empty bins.')
-    return
 
 
 def bin_index(phi, n_bins: int):
--- src/trion/analysis/utils.py
+++ src/trion/analysis/utils.py
@@ -1,2 +1,24 @@
+import pandas as pd
+import numpy as np
 
 
+def empty_bins_in(df: pd.DataFrame) -> None:
+    """Checks binned 1D or 2D DataFrame for empty bins in index (1D) and column names (2D).
+    Returns True if empty bins are detected."""
+
+    # check for missing tap bins
+    for n in range(df.shape[0]):
+        if df.index[n] != n:
+            raise ValueError('The binned DataFrame has missing bins.')
+
+    # check for missing pshet bins
+    if isinstance(df.columns, pd.MultiIndex):
+        for channel in df.columns.get_level_values(0).drop_duplicates():
+            for m in range(df[channel].shape[1]):
+                if df[channel].columns[m] != m:
+                    raise ValueError('The binned DataFrame has missing bins.')
+
+    # check for empty (NAN) bins
+    if np.isnan(df).any(axis=None):
+        raise ValueError('The binned DataFrame has empty bins.')
+    return
