97ae66055e5e34800345f37a43cbf1812487603f
pschwendke <33943413+pschwendke@users.noreply.github.com>
2022-06-07 21:23:21 +0200

    Feature acquisition routines (#12)
    * Move neaspec controller to `trion.expt.nea_ctrl`
    
    * Moved scans to `trion.expt.scans`. Uses ABC and inheritance.
    
    * Acquisition routines in `trion.expt.scans` now derive for base scans. It's overkill for now, but will be handy later.
    
    * Added `trion.analysis.io.export_gwy`.
    
    * Abusing Enums for Scan types (because we CAN!)

---
+++ src/trion/expt/nea_ctrl.py
@@ -0,0 +1,136 @@
+import sys
+import clr
+import logging
+from time import sleep
+import numpy as np
+
+# from trion.analysis.signals import Demodulation, NamedEnum
+from trion.analysis.signals import Demodulation
+
+# import the neaSpec SDK
+neaspec_folder = '//nea-server/updates/SDK'  # Import all DLLs in the folder or 'N:/updates/SDK' to mapped drive
+sys.path.append(neaspec_folder)  # Import all DLLs in the folder
+import Nea.Client.Hardware.SDK as neaSDK  # Import the DLL as element neaSDK
+clr.AddReference('Nea.Client.Hardware')  # Load the main DLL
+
+logger = logging.getLogger(__name__)
+
+
+def to_numpy(data: dict) -> dict:
+    """ Converts data, as returned from NeaSNOM.scan(), to numpy arrays.
+    (stolen from neaSpec SDK sample script)
+    """
+    for key in data.keys():
+        d2 = data[key].GetData()
+        limit1 = d2.GetUpperBound(0)+1
+        limit2 = d2.GetUpperBound(1)+1
+        array = np.zeros([limit1, limit2])
+        for a in range(limit1): 
+            for b in range(limit2):
+                array[a, b] = d2[a, b]
+        data[key] = array
+    return data
+
+
+class NeaSNOM:
+    def __init__(self):
+        # connects to the microscope of neaSNOM
+        # returns an instance of a microscope neaMIC and the connection to the SDK nea_client
+        self.scan = None
+        self.tracked_channels = None
+        self.tip_velocity = 5.                # tip velocity when moving to target position
+        self.nea_client = neaSDK.Connection('nea-server')               # Open up connection to microscope
+        self.nea_mic = self.nea_client.Connect()                # Define the Microscope
+        sleep(0.1)                              # Short delay makes things work fine (?)
+        self.stop()
+
+        logger.info('Microscope connected')
+        logger.debug("Client Version: " + self.nea_mic.ClientVersion)    # get Client Version
+        logger.debug("Server Version: " + self.nea_mic.ServerVersion)    # get Server Version
+
+    def engage(self, setpoint: float = 0.8):
+        logger.info('Engaging sample')
+        if not self.nea_mic.IsInContact:
+            self.nea_mic.AutoApproach(setpoint)
+        logger.info('Waiting a bit ...')
+        sleep(5)
+
+    def goto_xy(self, x, y):
+        assert 0 < x < 100, 'x coordinate out of bounds (0 < x < 100)'
+        assert 0 < y < 100, 'y coordinate out of bounds (0 < y < 100)'
+        reached = self.nea_mic.GotoTipPosition(x, y, self.tip_velocity)
+        if not reached:
+            raise RuntimeError('Did not reach target position')
+
+    def stop(self):
+        logger.info('Stopping Scan')
+        self.nea_mic.CancelCurrentProcedure()
+        self.nea_mic.RegulatorOff()
+
+    def disconnect(self):
+        logger.info('Disconnecting microscope')
+        self.stop()
+        self.nea_mic.GotoTipPosition(50, 50, 1e-3)
+        self.nea_client.Disconnect()
+
+    def set_pshet(self, mod: Demodulation):
+        logger.info(f'Settig NeaSNOM modulation to {mod.value}')
+        if mod == Demodulation.shd:
+            mod_setter = self.nea_mic.PrepareApproachCurveAfm()
+        elif mod == Demodulation.pshet:
+            mod_setter = self.nea_mic.PrepareApproachCurvePsHet()
+        mod_setter.Start()
+        sleep(0.1)
+        self.nea_mic.CancelCurrentProcedure()
+
+    def prepare_retraction(self, mod: Demodulation, z_size: float, z_res: int, afm_sampling_time: float):
+        self.tracked_channels = ['Z', 'M1A', 'M1P']
+        assert mod in [Demodulation.pshet, Demodulation.shd], f'{mod.value} is not implemented'
+
+        if mod == Demodulation.shd:
+            self.scan = self.nea_mic.PrepareApproachCurveAfm()
+        elif mod == Demodulation.pshet:
+            self.scan = self.nea_mic.PrepareApproachCurvePsHet()
+
+        self.scan.set_ApproachCurveHeight(z_size)
+        self.scan.set_ApproachCurveResolution(z_res)
+        self.scan.set_SamplingTime(afm_sampling_time)
+
+    def prepare_image(self, mod: Demodulation, x_center, y_center, x_size, y_size, x_res, y_res, angle, sampling_time):
+        self.tracked_channels = ['Z', 'R-Z', 'M1A', 'R-M1A', 'M1P', 'R-M1P']
+        assert mod in [Demodulation.pshet, Demodulation.shd], f'{mod.value} is not implemented'
+
+        step = x_size / x_res
+        vel = step / sampling_time * 1000
+        logger.info(f'Tip velocity is set to {vel:.2} um s^-1')
+        if vel > 4:
+            logger.warning('WARNING: tip velocity > 4 um s^-1')
+        if mod == Demodulation.pshet:
+            self.scan = self.nea_mic.PreparePsHetScan()
+        elif mod == Demodulation.shd:
+            self.scan = self.nea_mic.PrepareAfmScan()
+
+        self.scan.set_CenterX(x_center)
+        self.scan.set_CenterY(y_center)
+        self.scan.set_ScanAreaWidth(x_size)
+        self.scan.set_ScanAreaHeight(y_size)
+        self.scan.set_ResolutionColumns(x_res)
+        self.scan.set_ResolutionRows(y_res)
+        self.scan.set_ScanAngle(angle)
+        self.scan.set_SamplingTime(sampling_time)
+
+    def start(self):
+        image = self.scan.Start()
+        data = {ch: image.GetChannel(ch) for ch in self.tracked_channels}
+        return data
+
+    def get_current(self):
+        """
+        returns tuple (x, y, z, amp, phase)
+        """
+        x = self.nea_mic.TipPositionX * 1E-6  # all in m
+        y = self.nea_mic.TipPositionY * 1E-6
+        z = self.nea_mic.GetChannel('Z').CurrentValue * 1E-6
+        amp = self.nea_mic.GetChannel('M1A').CurrentValue * 1E-9
+        phase = self.nea_mic.GetChannel('M1P').CurrentValue
+        return x, y, z, amp, phase
---
+++ src/trion/expt/scans.py
@@ -0,0 +1,150 @@
+import os
+import logging
+import numpy as np
+import xarray as xr
+from abc import ABC, abstractmethod
+from datetime import datetime
+from time import sleep
+
+from trion.analysis.io import export_data
+from trion.expt.daq import DaqController
+from trion.expt.buffer import CircularArrayBuffer
+from trion.analysis.signals import Scan
+from trion.expt.nea_ctrl import NeaSNOM, to_numpy
+from trion.__init__ import __version__
+
+logger = logging.getLogger(__name__)
+
+
+class BaseScan(ABC):
+    def __init__(self, signals, mod, device='Dev1', clock_channel='pfi0'):
+        self.signals = signals
+        self.mod = mod
+        self.device = device
+        self.clock_channel = clock_channel
+        self.trion_version = __version__
+        self.afm_data = None
+        self.start_time = None
+        self.data_folder = None
+        self.neaclient_version = None
+        self.neaserver_version = None
+        self.stop_time = None
+        self.afm = None
+        self.afm_data = None
+        self.acquisition_mode = None
+        self.metalist = ['x_size', 'y_size', 'z_size', 'x_res', 'y_res', 'z_res', 'x_center', 'y_center', 'setpoint',
+                         'tip_velocity', 'afm_sampling_time', 'afm_angle', 'device', 'clock_channel', 'npts',
+                         'data_folder', 'trion_version', 'neaclient_version', 'neaserver_version']
+
+    def prepare(self):
+        logger.info('Preparing Scan')
+        self.connect()
+        self.start_time = datetime.now()
+
+        logger.info('Creating data folder')
+        self.data_folder = self.start_time.strftime('%y%m%d-%H%M%S_pixel_files')
+        try:
+            os.mkdir(self.data_folder)
+        except FileExistsError:
+            logger.warning('Data directory already exists. Previously recorded files will be overwritten.')
+
+    @abstractmethod
+    def connect(self):
+        """
+        connect to all relevant devices
+        """
+        pass
+
+    @abstractmethod
+    def disconnect(self):
+        """
+        disconnect from all relevant devices
+        """
+        pass
+
+    def export(self):
+        logger.info('Saving AFM data')
+        self.afm_data.attrs['date'] = self.start_time.strftime('%Y-%m-%dT%H:%M:%S')
+        self.afm_data.attrs['acquisition_time'] = str(self.stop_time - self.start_time)
+        self.afm_data.attrs['modulation'] = self.mod.value
+        self.afm_data.attrs['signals'] = [s.value for s in self.signals]
+        self.afm_data.attrs['acquisition_mode'] = self.acquisition_mode.value
+        for k, v in self.__dict__.items():
+            if k in self.metalist and v is not None:
+                self.afm_data.attrs[k] = v
+
+        if self.acquisition_mode in [Scan.stepped_image, Scan.continuous_image]:
+            self.afm_data.attrs['x_offset'] = (self.x_center - self.x_size / 2) * 1E-6
+            self.afm_data.attrs['y_offset'] = (self.y_center - self.y_size / 2) * 1E-6
+
+        filename = self.start_time.strftime('%y%m%d-%H%M%S_') + f'{self.acquisition_mode.value}.nc'
+        self.afm_data.to_netcdf(filename)
+
+
+class SteppedScan(BaseScan):
+    def connect(self):
+        logger.info('Connecting')
+        self.afm = NeaSNOM()
+        self.neaclient_version = self.afm.nea_mic.ClientVersion
+        self.neaserver_version = self.afm.nea_mic.ServerVersion
+
+    def disconnect(self):
+        logger.info('Disconnecting')
+        self.afm.disconnect()
+        self.stop_time = datetime.now()
+
+
+class ContinuousScan(BaseScan):
+    def __init__(self, signals, mod):
+        super().__init__(signals, mod)
+        self.ctrl = None
+        self.buffer = None
+        self.npts = None
+
+    def connect(self):
+        logger.info('Connecting')
+        self.afm = NeaSNOM()
+        self.neaclient_version = self.afm.nea_mic.ClientVersion
+        self.neaserver_version = self.afm.nea_mic.ServerVersion
+        self.ctrl = DaqController(self.device, clock_channel=self.clock_channel)
+        self.buffer = CircularArrayBuffer(vars=self.signals, size=100_000)
+        self.ctrl.setup(buffer=self.buffer)
+
+    def disconnect(self):
+        logger.info('Disconnecting')
+        self.afm.disconnect()
+        self.stop_time = datetime.now()
+        self.ctrl.close()
+
+    def acquire(self):
+        # TODO fix progress bars
+        logger.info('Starting scan')
+        self.ctrl.start()
+        nea_data = self.afm.start()
+
+        excess = 0
+        pix_count = 0
+        afm_tracking = []
+        while not self.afm.scan.IsCompleted:
+            print(f'Pixel no {pix_count}. Scan progress: {self.afm.scan.Progress * 100:.2f} %', end='\r')
+            afm_tracking.append(list(self.afm.get_current()))
+
+            n_read = excess
+            while n_read < self.npts:
+                sleep(0.001)
+                n = self.ctrl.reader.read()
+                n_read += n
+            excess = n_read - self.npts
+            data = self.buffer.get(n=self.npts, offset=excess)
+
+            export_data(f'{self.data_folder}/pixel_{pix_count:05d}.npz', data, self.signals)
+            pix_count += 1
+        print('\nScan complete')
+
+        afm_tracking = np.array(afm_tracking)
+        tracked_data = xr.DataArray(data=afm_tracking, dims=('px', 'ch'),
+                                    coords={'px': np.arange(pix_count), 'ch': ['x', 'y', 'Z', 'M1A', 'M1P']})
+        if self.acquisition_mode == Scan.continuous_image:
+            nea_data = to_numpy(nea_data)
+
+        return tracked_data, nea_data
