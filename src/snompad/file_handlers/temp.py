0ca78fdd294b109b18c4a9522e9cd2c2b3c0d2c9
unknown <mail@philippschwendke.de>
2024-06-17 14:49:06 +0200

    H5Buffer debugging

--- src/snompad/file_handlers/temp.py
+++ src/snompad/file_handlers/temp.py
@@ -1,28 +1,34 @@
 import tempfile
 import h5py
+import logging
 
+logger = logging.getLogger(__name__)
 
 class H5Buffer:
     """ This is just a wrapper to put a dictionary of 32-bit data arrays in a temporary hdf5 file.
     """
     def __init__(self):
-        tf = tempfile.TemporaryFile()
-        self.file = h5py.File(tf, 'w')
+        self.temp_file = tempfile.NamedTemporaryFile()
+        logger.info(f'H5Buffer: Created temporary file: {self.temp_file.name}')
+        self.h5_file = h5py.File(self.temp_file, 'w')
 
     def __getitem__(self, key):
-        return self.file[key]
+        return self.h5_file[key]
 
     def __setitem__(self, key, value):
-        self.file.create_dataset(key, data=value, dtype='float32')  # maybe 16 bit is even enough???
+        self.h5_file.create_dataset(str(key), data=value, dtype='float32')  # maybe 16 bit is even enough???
 
     def __del__(self):
+        logger.debug('H5Buffer: __del__()')
         self.cleanup()
 
     def keys(self):
-        return self.file.keys()
+        return self.h5_file.keys()
 
     def items(self):
-        return self.file.items()
+        return self.h5_file.items()
 
     def cleanup(self):
-        self.file.close()  # garbage collector closes tf
+        logger.info('H5Buffer: closing buffer')
+        self.h5_file.close()
+        self.temp_file.close()
